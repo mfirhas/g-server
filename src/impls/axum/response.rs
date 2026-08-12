054e0f4aba37f1d1180deaa548356f49e1bd9a07
mfirhas <fathir.irhas@gmail.com>
2026-08-12 11:26:48 +0700

    Add axum impl for request & response

---
+++ src/impls/axum/response.rs
@@ -0,0 +1,22 @@
+use crate::http::{Response, Status};
+
+impl From<Status> for axum::http::StatusCode {
+    fn from(status: Status) -> Self {
+        axum::http::StatusCode::from_u16(status.as_u16()).expect("invalid HTTP status code")
+    }
+}
+
+impl From<Response<axum::http::HeaderMap, axum::body::Body>>
+    for axum::http::Response<axum::body::Body>
+{
+    fn from(response: Response<axum::http::HeaderMap, axum::body::Body>) -> Self {
+        let (status, headers, body) = response.into_parts();
+
+        let mut response = axum::http::Response::new(body);
+
+        *response.status_mut() = status.into();
+        *response.headers_mut() = headers;
+
+        response
+    }
+}
