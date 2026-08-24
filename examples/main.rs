7405b9415bc3f5b490b9d724afd5337b109b3ff8
mfirhas <fathir.irhas@gmail.com>
2026-08-24 12:28:56 +0700

    Update skeleton with comments

--- examples/main.rs
+++ examples/main.rs
@@ -128,30 +128,49 @@ where
 // below this is macro generated for axum implementation
 // -----------------------------------------------------
 
+// We always include this in generated code for response.
 use g_server::{IntoAxumHtmlResponse, IntoAxumJsonResponse, IntoAxumStringResponse};
 
 #[tokio::main]
 async fn main() {
+    // MACRO: `app_a` comes from server/app name, e.g. http("app_a", "ip address", port)
     let app_a = __init_app_a();
+    // let another_app = __init_another_app();
 
+    // MACRO: in `app_a_listener`, the `app_a...` comes from server/app_name. Same for all `app_a` occurences
+    // For each server, we generate listener with name let `<server_name>_listener = ...`.
     let app_a_listener = ::tokio::net::TcpListener::bind((app_a.0.ip_address, app_a.0.port))
         .await
         .expect(format!("failed creating {} tcp listener", app_a.0.name).as_str());
+    // let another_app_listener =
+    //     ::tokio::net::TcpListener::bind((another_app.0.ip_address, another_app.0.port))
+    //         .await
+    //         .expect(format!("failed creating {} tcp listener", another_app.0.name).as_str());
 
+    // For each server, we generate log using its server name.
     println!(
         "g-server: running {} on {}:{}...",
         app_a.0.name, app_a.0.ip_address, app_a.0.port
     );
+    // println!(
+    //     "g-server: running {} on {}:{}...",
+    //     another_app.0.name, another_app.0.ip_address, another_app.0.port
+    // );
 
-    ::tokio::try_join!(::axum::serve(app_a_listener, app_a.1),)
-        .expect("failed running the all servers...");
+    // We join all servers inside this
+    ::tokio::try_join!(
+        ::axum::serve(app_a_listener, app_a.1),
+        // ::axum::serve(another_app_listener, another_app.1),
+    )
+    .expect("failed running the all servers...");
 }
 
+// function name comes from `__init_<server_name>`
 pub fn __init_app_a() -> (g_server::Server, axum::Router<()>) {
     let server = g_server::Server {
-        name: "app_a",
-        ip_address: "0.0.0.0",
-        port: 42069,
+        name: "app_a",         // from server name
+        ip_address: "0.0.0.0", // from server ip address
+        port: 42069,           // from server port
     };
 
     let global_config = g_server::Config::default();
@@ -159,16 +178,17 @@ pub fn __init_app_a() -> (g_server::Server, axum::Router<()>) {
     // global_config.timeout = Some(X); // X = user defined timeout from macro
     // ...
 
-    let context = Context::init(); // Context is from macro, appended with `::init()`
+    let context = Context::init(); // `Context` is from macro, appended with `::init()`
 
     let router = axum::Router::new();
 
-    // middlewares
+    // config middlewares, we define them if they're not None.
     // if let Some(timeout) = global_config.timeout {
     //     router = router.layer(TimeoutLayer::new(Duration::from_millis(timeout.into())));
     // }
 
     // we register all routes here
+    // We call all functions registering the router's handlers: __route_<server_name>_<handler_name>
     let router = __route_app_a_handler_1(router, global_config.clone());
     let router = __route_app_a_handler_2(router, global_config.clone());
 
@@ -177,6 +197,7 @@ pub fn __init_app_a() -> (g_server::Server, axum::Router<()>) {
     (server, router)
 }
 
+// function name comes from `__route_<server_name>_<handler_name>`
 pub fn __route_app_a_handler_1(
     router: axum::Router<Context>,
     global_config: g_server::Config,
@@ -189,23 +210,24 @@ pub fn __route_app_a_handler_1(
     // We assemble middlewares from last to first: first in array execute first, so we declare last here to make it executed first.
     // We assemble these middlewares directly from macro declaration.
     // If no middlewares, straight to route.
+    // `logger` and `auth` come from list of middlewares defined in macro declaration. We register them in reverse order, meaning first in list executed first, declare last here.
     let executor = g_server::route::Executor::new(move |cx, req| logger(cx, req, executor));
     let executor = g_server::route::Executor::new(move |cx, req| auth(cx, req, executor));
     let route = g_server::route::Route::<_> {
-        method: g_server::route::HttpMethod::Post,
+        method: g_server::route::HttpMethod::Post, // from macro: g_server::route::HttpMethod::$expr -> method
         endpoint: "/route_1/{user_id}/{user_email}",
         config,
-        response_body_type: g_server::route::ResponseBodyType::Json,
+        response_body_type: g_server::route::ResponseBodyType::Json, // from macro. g_server::route::ResponseBodyType::$expr
         executor: executor,
     };
 
     #[rustfmt::skip]
     let route_handler = move |
-          axum::extract::State(cx): axum::extract::State<Context>,
-          headers: axum::http::HeaderMap,
-          axum::extract::Path(path_params): axum::extract::Path<PathParams>,
-          axum::extract::Query(query_params): axum::extract::Query<QueryParams>,
-          axum::extract::Json(body): axum::extract::Json<RequestBody>,
+          axum::extract::State(cx): axum::extract::State<Context>, // `Context` comes from macro
+          headers: g_server::http::HeaderMap,
+          axum::extract::Path(path_params): axum::extract::Path<PathParams>, // `PathParams` comes from macro, if omitted becomes ()
+          axum::extract::Query(query_params): axum::extract::Query<QueryParams>, // `QueryParams` comes from macro, if omitted becomes ()
+          axum::extract::Json(body): axum::extract::Json<RequestBody>, // `RequestBody` comes from macro, if omitted becomes ()
     | async move {
         let req = g_server::Request {
             headers,
@@ -250,6 +272,8 @@ pub fn __route_app_a_handler_1(
     router
 }
 
+// another handler within same server,
+// same rules applied.
 pub fn __route_app_a_handler_2(
     router: axum::Router<Context>,
     global_config: g_server::Config,
