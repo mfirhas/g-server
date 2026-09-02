cc820f6bfb37b2183adb8d3cbea84fffbdcc1c9c
mfirhas <fathir.irhas@gmail.com>
2026-09-03 06:25:18 +0700

    edit

--- g-server-macro/src/axum_impl.rs
+++ g-server-macro/src/axum_impl.rs
@@ -59,48 +59,6 @@ pub(crate) fn expand(input: crate::server::GServer) -> Result<TokenStream2> {
     })
 }
 
-pub(crate) fn generate_body_extractor(body: &Option<RequestBody>) -> TokenStream2 {
-    match body {
-        // JSON body:
-        //
-        // request_body: Json(MyStruct)
-        Some(RequestBody::Json(ty)) => {
-            quote! {
-                ::axum::extract::Json(body):
-                    ::axum::extract::Json<#ty>,
-            }
-        }
-
-        // Form body:
-        //
-        // request_body: Form(MyStruct)
-        Some(RequestBody::Form(ty)) => {
-            quote! {
-                ::axum::extract::Form(body):
-                    ::axum::extract::Form<#ty>,
-            }
-        }
-
-        // String body:
-        //
-        // request_body: String
-        Some(RequestBody::String) => {
-            quote! {
-                body: String,
-            }
-        }
-
-        // No request_body:
-        //
-        // body is simply ().
-        None => {
-            quote! {
-                body: (),
-            }
-        }
-    }
-}
-
 // ============================================================
 // main()
 // ============================================================
@@ -377,6 +335,48 @@ fn generate_route_function(
             #registration
         }
     })
+}
+
+pub(crate) fn generate_body_extractor(body: &Option<RequestBody>) -> TokenStream2 {
+    match body {
+        // JSON body:
+        //
+        // request_body: Json(MyStruct)
+        Some(RequestBody::Json(ty)) => {
+            quote! {
+                ::axum::extract::Json(body):
+                    ::axum::extract::Json<#ty>,
+            }
+        }
+
+        // Form body:
+        //
+        // request_body: Form(MyStruct)
+        Some(RequestBody::Form(ty)) => {
+            quote! {
+                ::axum::extract::Form(body):
+                    ::axum::extract::Form<#ty>,
+            }
+        }
+
+        // String body:
+        //
+        // request_body: String
+        Some(RequestBody::String) => {
+            quote! {
+                body: String,
+            }
+        }
+
+        // No request_body:
+        //
+        // body is simply ().
+        None => {
+            quote! {
+                body: (),
+            }
+        }
+    }
 }
 
 // ============================================================
