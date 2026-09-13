2e85465923a48ca541d205791211172425c4a74c
mfirhas <fathir.irhas@gmail.com>
2026-09-13 11:02:17 +0700

    refactor

--- g-server-macro/src/server.rs
+++ g-server-macro/src/server.rs
@@ -225,13 +225,16 @@ fn validate_group_configs(group: &crate::group::Group) -> Result<()> {
     Ok(())
 }
 
+// configs that only exist at global config
+static GLOBAL_CONFIG: [&str; 1] = ["normalize_endpoint"];
+
 fn validate_non_global_configs(configs: &[ConfigEntry]) -> Result<()> {
     for cfg in configs {
         let ident_name = cfg.name.to_string();
-        if ident_name == "normalize_endpoint" {
+        if GLOBAL_CONFIG.contains(&ident_name.as_str()) {
             return Err(syn::Error::new(
                 cfg.name.span(),
-                "`normalize_endpoint` only allowed for global config",
+                format!("`{}` only allowed for global config", &ident_name),
             ));
         }
     }
